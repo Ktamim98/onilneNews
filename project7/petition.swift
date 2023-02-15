edc87aca01fd087eff58ab0a513656869fcc97a6
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-02-16 01:09:51 +0600

    Initial Commit

---
+++ project7/petition.swift
@@ -0,0 +1,14 @@
+//
+//  petition.swift
+//  project7
+//
+//  Created by Tamim Khan on 15/2/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title : String
+    var body : String
+    var signatureCount : Int
+}
---
+++ project7/petitions.swift
@@ -0,0 +1,12 @@
+//
+//  petitions.swift
+//  project7
+//
+//  Created by Tamim Khan on 15/2/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results:  [Petition]
+}
