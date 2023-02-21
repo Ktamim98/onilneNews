4bbe1f6d27cbd47958abca6fdb69f2bc6b9c078e
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-02-21 20:31:26 +0600

    .update with performSelector on background

--- project7/ViewController.swift
+++ project7/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 class ViewController: UITableViewController {
     
     
-    var petitions = [Petition]()
+  var petitions = [Petition]()
     
-    
+   var filteredPetitions = [Petition]()
     
     
     override func viewDidLoad() {
@@ -26,13 +26,16 @@ class ViewController: UITableViewController {
         
         
         let filterButton = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterResults))
+        let resetButton = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetList))
+        
+        navigationItem.leftBarButtonItems = [filterButton, resetButton]
+        
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
         
         
-       
         
-        navigationItem.leftBarButtonItems = [filterButton]
-        
-        
+    @objc func fetchJSON(){
         let urlString: String
         if navigationController?.tabBarItem.tag == 0{
             
@@ -45,28 +48,37 @@ class ViewController: UITableViewController {
                 parse(json: data)
                 return
             }
-            showError()
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+    
+    func parse(json: Data){
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        }else{
             
-            func showError(){
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+        
+    }
+          @objc func showError(){
                 let ac = UIAlertController(title: "Loading Error", message: "There is a problem while loading; please check your internet and try later.", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
                 present(ac, animated: true)
             }
             
-            func parse(json: Data){
-                let decoder = JSONDecoder()
-                
-                if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
-                    petitions = jsonPetitions.results
-                    tableView.reloadData()
-                }
-               
-            }
-           
-        }
-        
-       
+            
+    @objc func showSource(){
+        let ScoreAlert = UIAlertController(title: "CREDITS!", message: "The data comes from, We The People API of the Whitehouse.", preferredStyle: .alert)
+        ScoreAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ScoreAlert, animated: true)
     }
+    
+        
+        
    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
@@ -75,10 +87,6 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        
-        
-        
         let petition = petitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
@@ -91,25 +99,15 @@ class ViewController: UITableViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    @objc func showSource(){
-        let ScoreAlert = UIAlertController(title: "CREDITS!", message: "The data comes from, We The People API of the Whitehouse.", preferredStyle: .alert)
-        ScoreAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(ScoreAlert, animated: true)
-    }
 
     
     
-    func filterResultsIn(_ answer: String) {
+  @objc func showPetintion(_ answer: String) {
         
-        
-        var filteredPetitions = [Petition]()
-        
-        
-        filteredPetitions = petitions.filter { $0.title.contains(answer) || $0.body.contains(answer)
+      filteredPetitions = petitions.filter { $0.title.contains(answer) || $0.body.contains(answer)
         }
-        
-        petitions = filteredPetitions
-        tableView.reloadData()
+      petitions = filteredPetitions
+      tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
     }
     
     
@@ -121,13 +119,22 @@ class ViewController: UITableViewController {
             [weak self, weak ac] action in
             guard let answer = ac?.textFields?[0].text
             else { return }
-            self?.filterResultsIn(answer)
+            self?.performSelector(inBackground: #selector(self?.showPetintion), with: answer)
+          
+            
         }
         ac.addAction(submitFilter)
         present(ac, animated: true)
         
     }
-    
+    @objc func resetList() {
+        DispatchQueue.main.async {
+            self.petitions = self.filteredPetitions
+            self.tableView.reloadData()
+        }
+
+    }
+
             
         }
         
